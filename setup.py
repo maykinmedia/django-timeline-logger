4a07b93a8b37905dbd18cfc230ff5d9853874963
José L. Patiño <jose.lpa@gmail.com>
2016-06-14 13:15:35 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-timeline-logger',
+    version='0.1a',
+    description='Generic event logger for Django models.',
+    author='MaykinMedia Team staff',
+    author_email='team@maykinmedia.nl',
+    url='https://www.maykinmedia.nl',
+    install_requires=[
+        'Django>=1.7',
+    ],
+)
