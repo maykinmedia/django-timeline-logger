745fefc508ad11938a219401b957762c97a04518
José L. Patiño <jose.lpa@gmail.com>
2016-06-15 19:00:20 +0100

    Added `log_from_request` and `get_message` methods to `TimelineLog`.

--- timeline_logger/models.py
+++ timeline_logger/models.py
@@ -1,10 +1,17 @@
+import logging
+
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.postgres.fields import JSONField
 from django.db import models
+from django.template.loader import render_to_string
 from django.utils.encoding import python_2_unicode_compatible
 
 from .conf import settings
+
+
+logger = logging.getLogger('timeline_logger')
 
 
 @python_2_unicode_compatible
@@ -16,3 +23,41 @@ class TimelineLog(models.Model):
     extra_data = JSONField(null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     action = models.CharField(max_length=50, choices=settings.TIMELINE_ACTIONS)
+
+    @classmethod
+    def log_from_request(cls, request, content_object, action, **extra_data):
+        """
+        Given an ``HTTPRequest`` object and a generic content, it creates a
+        ``TimelineLog`` object to store the data of that request.
+
+        :param request: A Django ``HTTPRequest`` object.
+        :param content_object: A Django model instance. Any object can be
+        related.
+        :param action: One of the choices of ``settings.TIMELINE_ACTIONS``.
+        :param extra_data: A dictionary (translatable into JSON) determining
+        specifications of the action performed.
+        :return: A newly created ``TimelineLog`` instance.
+        """
+        user = request.user
+        user = user if isinstance(user, User) else None
+
+        timeline_log = cls.objects.create(
+            content_object=content_object,
+            extra_data=extra_data or None,
+            user=user,
+            action=action
+        )
+        logger.debug('Logged event in {0} {1}.'.format(
+            content_object._meta.object_name, content_object.pk))
+
+        return timeline_log
+
+    def get_message(self, template):
+        """
+        Gets the 'log' message, describing the event performed.
+
+        :param template: String representing the template to be used to render
+        the message.
+        :return: The log message string.
+        """
+        return render_to_string(template, {'log': self})
