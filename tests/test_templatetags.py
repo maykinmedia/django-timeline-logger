995e06c51b89610b21b9f8a88cd1b61f5b4fbcde
Sergei Maertens <sergei@maykinmedia.nl>
2016-06-24 18:19:20 +0200

    Skip test if not on PostgreSQL

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -3,13 +3,18 @@ try:
 except ImportError:
     import mock
 
-import pytest
-
+from django.conf import settings
 from django.template.loader import render_to_string
+
+import pytest
 
 from .factories import TimelineLogFactory
 
 
+@pytest.mark.skipif(
+    settings.DATABASES['default']['ENGINE'] != 'django.db.backends.postgresql',
+    reason='Requires PostgreSQL'
+)
 @pytest.mark.django_db
 def test_render_message():
     log = TimelineLogFactory.create(extra_data={'foo': 'bar'})
