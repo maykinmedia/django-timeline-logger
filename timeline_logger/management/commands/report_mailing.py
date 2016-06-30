a67cfd0e0e9d0840b113fb771829ac0963a46eaf
José L. Patiño <jose.lpa@gmail.com>
2016-06-30 16:11:24 +0100

    Fixed? nasty interversions import stuff.

--- timeline_logger/management/commands/report_mailing.py
+++ timeline_logger/management/commands/report_mailing.py
@@ -1,6 +1,20 @@
-import html
 import logging
+import sys
 from datetime import timedelta
+
+
+# Safely importing the HTML parser utility, highly dependent on Python versions
+if sys.version_info >= (3, 4):
+    # From v3.4.0 in advance
+    import html
+elif (3, 0) < sys.version_info < (3, 4):
+    # From v3.0 until 3.3
+    from html.parser import HTMLParser
+    html = HTMLParser()
+else:
+    # Python 2.x versions
+    from HTMLParser import HTMLParser
+    html = HTMLParser()
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
