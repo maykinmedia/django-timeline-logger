ce564e2a76f5363d963e2eafe9b2a9dea2adc554
Sergei Maertens <sergei@maykinmedia.nl>
2018-03-16 17:33:36 +0100

    Set up exports with a custom format

--- demo/demo_app/admin.py
+++ demo/demo_app/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 
 from import_export.admin import ExportMixin
+from import_export.formats import base_formats
+from import_export_xml.formats import XML
 from timeline_logger.models import TimelineLog
 from timeline_logger.admin import TimelineLogAdmin
+from timeline_logger.resources import TimelineLogResource
 
 admin.site.unregister(TimelineLog)
 
 
 @admin.register(TimelineLog)
 class TimelineLogAdmin(ExportMixin, TimelineLogAdmin):
-    pass
+    formats = (
+        XML,
+        base_formats.CSV,
+        base_formats.XLS,
+        base_formats.XLSX,
+        base_formats.TSV,
+        base_formats.ODS,
+        base_formats.JSON,
+        base_formats.YAML,
+        base_formats.HTML,
+    )
+    resource_class = TimelineLogResource
